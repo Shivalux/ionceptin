aa60d127611f01281a8716dc0af03db460901b13
shivalux <shivarakii@Shivaluxs-MacBook-Pro.local>
2023-05-11 04:09:43 +0700

    wordpress: clear

--- srcs/requirements/wordpress/tools/wordpress_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_script.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# * looping util mariadb container finish setting up
+# * looping util mariadb container finish setting up ;
 mysql -u $MYSQL_USERNAME_ADMIN -p$MYSQL_PASSWORD_ADMIN
 while [ $? -eq 1 ]; do
 	mysql -u $MYSQL_USERNAME_ADMIN -p$MYSQL_PASSWORD_ADMIN
-	echo wait...
 done
 
-wp-cli core download --allow-root;
+# * create config file for wordpress to passing thought wordpress website ;
 wp-cli config create --dbname=$MYSQL_NAME --dbuser=$MYSQL_USERNAME_ADMIN --dbpass=$MYSQL_PASSWORD_ADMIN --dbhost=$MYSQL_HOST --allow-root;
-wp-cli core install --url=sharnvon.42.fr --title=HELLO_WORLD --admin_user=$MYSQL_USERNAME_ADMIN --admin_email=sharnvon@42bangkok.com --allow-root;
-
-
+wp-cli core install --url=sharnvon.42.fr --title=$WEB_TITLE --admin_user=$MYSQL_USERNAME_ADMIN --admin_email=$WEB_EMAIL --allow-root;
